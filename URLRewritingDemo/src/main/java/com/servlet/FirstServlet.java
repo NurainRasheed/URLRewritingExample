1c9c015ade1284f85dad925137b0ec9c1b12f707
NurainRasheed <88193648+NurainRasheed@users.noreply.github.com>
2021-08-26 17:38:50 +0530

    Add files via upload

---
+++ URLRewritingDemo/src/main/java/com/servlet/FirstServlet.java
@@ -0,0 +1,45 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			response.setContentType("text/html");
+			PrintWriter out=response.getWriter();
+			String name=request.getParameter("uname");
+			out.print("<br>Welcome " + name);
+			
+			out.print("<a href='next?uname="+name+"'>visit</a>");
+			
+			out.close();
+		
+			
+			
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+}}
---
+++ URLRewritingDemo/src/main/java/com/servlet/SecondServlet.java
@@ -0,0 +1,44 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			response.setContentType("text/html");
+		
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("uname");
+		out.print("<br>Hello " + name);
+		out.close();
+			
+	}catch(Exception e) {
+		System.out.println(e);
+}
+	}
+}
